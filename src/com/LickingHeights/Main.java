7268b10751c3de5b420fc57d7f20fdc8b11b6ccd
Tyler <tyler.willison.2022@student.lhschools.org>
2019-11-14 09:42:02 -0500

    got the ints done and number generator

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,20 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello");
+	System.out.println("This is the number guessing game!");
+	int guess;
+	int answer;
+	answer = (int) (Math.random() * 100);
+
+
+
+
+			)
+
+    }
+}
