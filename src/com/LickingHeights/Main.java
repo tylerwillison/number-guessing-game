b58a4670c636c7e0ff969207160d8ce1bf79d52d
Tyler <tyler.willison.2022@student.lhschools.org>
2019-11-18 09:45:20 -0500

    did the too high , too low, and right ints set up

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -18,7 +18,19 @@ public class Main {
     keyboard = new Scanner(System.in);
     guess = keyboard.nextInt();
 
-    
+    do (
+        System.out.println("Guess a number");
+        guess = keyboard.nextInt();
+        if (guess>answer){
+            System.out.println("Too high, go again.");
+        }
+            else if (guess <answer){
+            System.out.println("Too low, go again.");
+        }
+            else if (guess == answer){
+            System.out.println("That is correct!");
+        }
+            )
 
 
 
